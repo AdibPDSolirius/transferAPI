860920e8a851d156aed74caecb785bfed180dfb4
Adib Pournazari <adib.pournazari@solirius.com>
2019-07-15 13:29:42 +0100

    Remove unused parameters

--- src/main/java/com/revolut/transfer/api/response/ResponseParameters.java
+++ src/main/java/com/revolut/transfer/api/response/ResponseParameters.java
@@ -5,9 +5,6 @@ public class ResponseParameters {
     private int httpStatus;
     private ResponseDTO responseDTO;
 
-    private String status;
-    private String message;
-
     public ResponseParameters(int httpStatus, ResponseDTO responseDTO) {
         this.httpStatus = httpStatus;
         this.responseDTO = responseDTO;
